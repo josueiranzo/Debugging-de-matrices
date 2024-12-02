f98424a33f03489523a5a57d6ee3dce0bf1802a3
A36-PC26\alumnat <a@a.com>
2024-12-02 16:22:14 +0100

    corregido

--- MatrixDebugging.java
+++ MatrixDebugging.java
@@ -1,10 +1,9 @@
-
 public class MatrixDebugging {
 
 	public static void main(String[] args) {
 		// Paso 1: Crear una matriz 3x3
         int[][] matrix = {
-            {1, 2, 3},
+            {1, 2, 3}, 	 	 	
             {4, 5, 6},
             {7, 8, 9}
         };
@@ -18,9 +17,11 @@ public class MatrixDebugging {
             System.out.println();
         }
 
-        // Paso 2: Intentar girar la matriz 90 grados
+        // Paso 2: Intentar transponer la matriz
+        // Intento de transposición de la matriz
         int[][] transposedMatrix = new int[matrix[0].length][matrix.length];
         
+        // Error en el bucle de transposición: usamos mal los índices
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 transposedMatrix[i][j] = matrix[j][i]; // Error: El índice i y j están invertidos
@@ -38,7 +39,7 @@ public class MatrixDebugging {
 
         // Paso 3: Intentar acceder a un índice fuera de los límites
         try {
-            System.out.println("\nAccediendo al elemento en [3][0]: " + matrix[3][0]);
+            System.out.println("\nAccediendo al elemento en [2][0]: " + matrix[2][0]); // Índice válido
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("\nError: " + e.toString());
         }
@@ -46,9 +47,8 @@ public class MatrixDebugging {
         // Paso 4: Intentar realizar un cálculo incorrecto con la matriz
         try {
             int sum = 0;
-            // Intento de sumar los elementos de la matriz de forma incorrecta
-            for (int i = 0; i <= matrix.length; i++) {  // Error: índice fuera de límites
-                for (int j = 0; j <= matrix[i].length; j++) {  // Error: índice fuera de límites
+            for (int i = 0; i < matrix.length; i++) {  // Usar < en lugar de <=
+                for (int j = 0; j < matrix[i].length; j++) {  // Usar < en lugar de <=
                     sum += matrix[i][j];
                 }
             }
@@ -56,6 +56,10 @@ public class MatrixDebugging {
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("\nError al intentar sumar: " + e.toString());
         }
+
 	}
 
 }
+
+
+
